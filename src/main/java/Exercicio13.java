12d5d61c993c5ba91b99fdd09c3464160ce28b32
root <root@DELL-7559>
2024-09-18 20:48:16 -0300

    Início do projeto

---
+++ src/main/java/Exercicio13.java
@@ -0,0 +1,32 @@
+// IMPORTS
+
+import java.util.Scanner;
+
+public class Exercicio13 {
+
+	public static void main(String[] args) {
+		// INSTANTIATE
+		Scanner sc = new Scanner(System.in);
+
+		// DECLARATE
+		double s;
+		double p;
+		double result;
+
+		// COLLECT DATA
+		System.out.print("S : ");
+		s = sc.nextDouble();
+		System.out.print("P : ");
+		p = sc.nextDouble();
+
+		// PROCESSING
+		result = s * 0.3;
+
+		// OUTPUT
+		if (p <= result) {
+			System.out.println("YES");
+		} else {
+			System.out.println("NO");
+		}
+	}
+}
